89194f7b3b8c1b16e96d9223a36c2d59067f8a58
azulita07 oviedo <zulayoea@gmail.com>
2023-09-01 00:54:58 +0000

    cambio1

---
+++ index.php
@@ -0,0 +1,20 @@
+
+
+<html>
+  <head>
+        <title>php contact form</title>
+  </head>
+    <body>
+            
+        <h1>php contact form</h1>
+
+        <form method="post" action="">
+
+        email address:
+        <br>
+        <input type="text" name="email" value="<?php echo $email; ?>">
+
+
+        </form>
+    </body>
+</html>
